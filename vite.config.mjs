5dd4ac527dabb924271c0b1e2477ab2921db1222
nnfzhomepc <mr.botnikoff@ya.ru>
2026-09-21 00:58:55 +0500

    asdc

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { readFileSync } from 'fs';
-
-const pkg = JSON.parse(readFileSync('./package.json', 'utf-8'));
+import pkg from './package.json';
 
 const host = process.env.TAURI_DEV_HOST;
 
